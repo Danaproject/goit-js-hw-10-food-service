38e23ed9f87332f119845ac25a42ba545423abd3
Dana Project <decoratio@gmail.com>
2021-02-06 21:29:07 +0100

    improved theme.js

--- src/theme.js
+++ src/theme.js
@@ -11,24 +11,17 @@ switcherRef.addEventListener('click', handleSwitcher);
 setSavedTheme();
 
 function handleSwitcher() {
-    let newTheme = Theme.DARK;
-
     if (bodyRef.classList.contains(Theme.LIGHT)) {
-        newTheme = Theme.DARK;
-    } else if (bodyRef.classList.contains(Theme.DARK)) {
-        newTheme = Theme.LIGHT;
-    }
-    localStorage.setItem('theme', newTheme);
-    toggleTheme(newTheme);
-}
-
-function toggleTheme(theme) {
-    if (theme === Theme.LIGHT) {
-        bodyRef.classList.remove(Theme.DARK);
-        bodyRef.classList.add(Theme.LIGHT);
-    } else if (theme === Theme.DARK) {
         bodyRef.classList.remove(Theme.LIGHT);
         bodyRef.classList.add(Theme.DARK);
+        localStorage.setItem('theme', Theme.DARK);
+    } else if (bodyRef.classList.contains(Theme.DARK)) {
+        bodyRef.classList.remove(Theme.DARK);
+        bodyRef.classList.add(Theme.LIGHT);
+        localStorage.setItem('theme', Theme.LIGHT);
+    } else { 
+        bodyRef.classList.add(Theme.DARK);
+        localStorage.setItem('theme', Theme.DARK);
     }
 }
 
@@ -37,5 +30,3 @@ function setSavedTheme() {
     if (savedTheme) bodyRef.classList.add(savedTheme); 
     if (savedTheme === Theme.DARK) switcherRef.checked = true;
 }
-
-
